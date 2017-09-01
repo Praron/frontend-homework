2bd3ce87196ab537c42e9b4b4aa40fc0bf1a1f2e
Praron <koctr123@mail.ru>
2017-09-01 16:23:58 +0300

    Реализовал inverse().

---
+++ source/inverse.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const inverse = (array, n = 0) => {   
+    if (n >= 0) {
+        return array.slice(0, n).concat(array.slice(n).reverse());
+    }
+    else {
+        return array.slice(0, n).reverse().concat(array.slice(n));
+    }
+}
